077122b49ea2eb1b7503fcd4255e98354ea469ba
Elder Nogueira <elder.nogueirasilva1@gmail.com>
2021-02-17 14:49:06 -0300

    rotas: incluindo tratamento de erros para rotas não existentes

--- app.js
+++ app.js
@@ -12,4 +12,19 @@ app.use(bodyParser.json());
 app.use('/clientes', rotaClientes);
 app.use('/wishlist', rotaWishlist);
 
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
 module.exports = app;
