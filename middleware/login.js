f82066d43d66628732904d7937ca3827dac95c50
Elder Nogueira <elder.nogueirasilva1@gmail.com>
2021-02-17 19:37:14 -0300

    protegendo as rotas: implementação de autorização e autenticação nas rotas de cliente e wishlist

---
+++ middleware/login.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const JWT_KEY = require('../jwt_key.json');
+
+exports.obrigatorio = (req, res, next) => {
+
+    try{
+        const token = req.headers.authorization.split(' ')[1]
+        const decode = jwt.verify(token, JWT_KEY.chave);
+        req.usuario = decode;
+        next();
+    } catch (error) {
+        return res.status(401).send({ mensagem: 'Falha na autenticação '})
+    }
+}
+
+exports.opcional = (req, res, next) => {
+
+    try{
+        const token = req.headers.authorization.split(' ')[1]
+        const decode = jwt.verify(token, JWT_KEY.chave);
+        req.usuario = decode;
+        next();
+    } catch (error) {
+        next();
+    }
+}
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const login = require('../middleware/login');
 
 const wishListController = require('../controllers/wishlist-controller');
 
-router.post('/', wishListController.postWishlist);
+router.post('/', login.obrigatorio, wishListController.postWishlist);
 
-router.get('/:id_cliente', wishListController.getClienteWishlist);
+router.get('/:id_cliente', login.obrigatorio, wishListController.getClienteWishlist);
 
-router.delete('/', wishListController.deleteProductWishList);
+router.delete('/', login.obrigatorio, wishListController.deleteProductWishList);
 
 module.exports = router;
